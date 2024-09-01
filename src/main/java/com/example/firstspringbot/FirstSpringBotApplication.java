8c775b3f91f71a28973d6a723dfdac8a6df53078
Alexander Savelev <alex.savelev21@yandex.ru>
2024-09-01 12:35:41 +0500

    base settings

---
+++ src/main/java/com/example/firstspringbot/FirstSpringBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstspringbot/FirstSpringBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
